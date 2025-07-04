9af734f88653d37744241bba52173990462ea405
이정헌 <aoc05230@naver.com>
2025-07-04 16:51:38 +0900

    section 4 finish

--- src/test/java/hello/hello_spring/service/MemberServiceTest.java
+++ src/test/java/hello/hello_spring/service/MemberServiceTest.java
@@ -1,22 +1,64 @@
 package hello.hello_spring.service;
 
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemoryMemberRepository;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+
 class MemberServiceTest {
 
-    @Test
-    void 회원가입() { // 테스트 작성시 한글사용도 OK
-        // given
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
 
-        // when
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
 
-        // then
-
+    @AfterEach
+    public void afterEach() {
+        memberRepository.clearStore();
     }
 
     @Test
-    void findMembers() {
+    void 회원가입() { // 테스트 작성시 한글사용도 OK
+
+        // given
+        Member member = new Member();
+        member.setName("hello");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        Member findMember = memberRepository.findById(saveId).get();
+        Assertions.assertEquals(member.getName(), findMember.getName());
+    }
+
+    @Test
+    void 중복_회원_예외() {
+
+        // Given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        // When
+        memberService.join(member1);
+        IllegalStateException e = Assertions.assertThrows(IllegalStateException.class,
+                () -> memberService.join(member2));
+
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+        // Than
     }
 
     @Test
